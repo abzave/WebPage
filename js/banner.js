522f23575972471d8b8c7124b7474d6a0ef86390
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-01-23 20:50:08 -0600

    [New] Information label for banner

--- js/banner.js
+++ js/banner.js
@@ -1,4 +1,6 @@
 var imagesUrls = ["D:/Pictures/Pic_20171107_203753_4096x2160.png", "D:/Pictures/Pic_20170706_203225_4096x2160.png", "D:/Pictures/148221.jpg"];
+var imageTitles = ["The Crew", "The Crew 2", "Need For Speed"];
+var imageDescriptions = ["Image from The Crew", "Another image from The Crew", "Image from Need For Speed"];
 
 $(document).ready(() => {
     var currentIndex = 0;
@@ -20,5 +22,8 @@ function showImage(imageIndex) {
     $(".bannerImage").css({"display": "block", "background": "url(" + imagesUrls[imageIndex] + ")", 
                            "background-position": "center center", "background-size": "cover", 
                            "background-repeat": "no-repeat"});
+    $(".imageInfo h2").html(imageTitles[imageIndex]);
+    $(".imageInfo p").html(imageDescriptions[imageIndex]);
+
     return imageIndex;
 }
