41230e36eefaf319b3aa4bd722f5c74fe33412e8
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-02-26 20:19:51 -0600

    [Updated] Project div redirect on click

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,11 @@
 $(document).ready(() => {
-    $(".projects .projectsShowcase .project").hover(function(){
+    var projects = $(".projects .projectsShowcase .project");
+    projects.hover(function(){
         $(this).css("opacity", "0.5");
     }, function(){
         $(this).css("opacity", "1");
+    });
+    projects.click(function() {
+        window.location.href = $(this).find(".link").attr("href");
     });
 });
