76e0ccdc88eff9a9445c13dc1ea3ae507107e693
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-04-12 16:49:31 -0600

    [Updated] Database indexing

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $databaseDriver = 'mysql';
+    $host = 'host=database_host';
+    $blogDatabase = 'blog';
+    $blogUser = 'database_user';
+    $blogPassword = 'database_password';
+
+?>
