d468b6a1fa3f630dd4fec91cf15f8f22a4f24843
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-04-12 18:07:12 -0600

    [Updated] Database connection

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php 
 
-    $databaseDriver = 'mysql';
-    $host = 'host=database_host';
-    $blogDatabase = 'blog';
-    $blogUser = 'database_user';
-    $blogPassword = 'database_password';
+    interface Config{
+        const DATABASE_DRIVE = 'mysql';
+        const HOST = 'your_host';
+        const CHARSET = 'UTF8';
+    }
 
 ?>
---
+++ php/databaseConnect.php
@@ -0,0 +1,18 @@
+<?php
+
+    require('config.php');
+
+    class Connect{
+        public static function getConnection($database, $user, $password){
+            try{
+                $connection = new PDO(Config::DATABASE_DRIVE . ":host=" . Config::HOST . "; dbname={$database}", $user, $password);
+                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $connection->exec("SET CHARACTER SET " . Config::CHARSET);
+            }catch(Exception $e){
+                die($e->getMessage());
+            }
+            return $connection;
+        }
+    }
+
+?>
