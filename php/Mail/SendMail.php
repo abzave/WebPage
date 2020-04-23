13ec98e5a1ebdae417e64cd9ec3ad48c7a0c4815
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-04-22 19:54:06 -0600

    [Updated] Send mail validation

--- php/Mail/SendMail.php
+++ php/Mail/SendMail.php
@@ -1,8 +1,30 @@
 <?php
 
     require_once("MailSender.php");
-    $sender = MailSender::getInstance();
-    $sender->send($_POST["name"] . " " . $_POST["lastname"], $_POST["email"], $_POST["message"]);
-    header("location:" . $_SERVER['HTTP_REFERER']);
+
+    if(isset($_POST["name"]) && isset($_POST["lastname"]) && isset($_POST["email"]) && isset($_POST["message"])){
+        $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+        $lastname = filter_var($_POST["lastname"], FILTER_SANITIZE_STRING);
+        $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+        $mail = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+        if(!empty($name) && !empty($lastname) && !empty($message) && !empty($_POST["email"]) && !empty($mail)){
+            $sender = MailSender::getInstance();
+            $sender->send($name . " " . $lastname, $mail, $message);
+            header("location:" . $_SERVER['HTTP_REFERER']);
+        }
+    }   
+
+    echo "<html>\n" . 
+            "\t<head>\n" . 
+                "\t\t<style>\n" . 
+                    "\t\t\tbody{\n" . 
+                        "\t\t\t\tbackground-color: #262626;\n" . 
+                    "\t\t\t}\n" . 
+                "\t\t</style>\n" . 
+            "\t</head>\n" . 
+            "\t<body>\n" . 
+                "\t\t<h1 align='center' style='color: #F9F871; margin-top: 45vh;'>Error al enviar el mensaje :(</h1>\n" . 
+            "\t\t</body>\n" . 
+        "</html>";
 
 ?>
